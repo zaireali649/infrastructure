0a23888a0df6d6004a006377f2cc29222c63cc74
Zaire Ali <zaireali649@gmail.com>
2025-08-26 22:33:17 -0400

    Update versions.tf

--- terraform/module/sagemaker-studio/versions.tf
+++ terraform/module/sagemaker-studio/versions.tf
@@ -1,10 +1,3 @@
-terraform {
-  required_version = ">= 1.5"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.0"
-    }
-  }
+provider "aws" {
+  region = "us-east-1"
 }
