f9070bf51bc085eff197db39fc7a9941f27a6ba7
Zaire Ali <zaireali649@gmail.com>
2025-08-29 11:09:39 -0400

    Update main.tf

--- sagemaker-pipelines/terraform/staging/main.tf
+++ sagemaker-pipelines/terraform/staging/main.tf
@@ -94,7 +94,7 @@ module "sagemaker_pipelines" {
 
   # S3 configuration
   s3_bucket_arn             = data.aws_s3_bucket.ml_bucket.arn
-  input_data_s3_path        = ""  # Not used - dataset loaded in code
+  input_data_s3_path        = ""  # Not used - self-contained training
   model_output_s3_path      = "s3://${local.s3_bucket_name}/iris/models/"
   inference_input_s3_path   = "s3://${local.s3_bucket_name}/iris/inference-input/"
   inference_output_s3_path  = "s3://${local.s3_bucket_name}/iris/inference-output/"
